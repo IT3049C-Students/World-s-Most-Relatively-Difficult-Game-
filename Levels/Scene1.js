f2e364e873a88fb69297a7a33328c0e5e32f5b00
SamuelUntener <75641765+SamuelUntener@users.noreply.github.com>
2022-04-14 13:40:21 -0400

    updated scene1

--- Levels/Scene1.js
+++ Levels/Scene1.js
@@ -10,12 +10,14 @@ export class Scene1 extends Phaser.Scene1 {
 preload() {
     this.load.image("Tiles", "src/Tiles/wholesheet1.png");
     this.load.tilemapTiledJSON('map','src/Json/tutorial..tmj')
+    //this.wallsLayer.setCollisionByProperty({ collides: true});
 }
 create() {
-const map = this.make.tilemap({key:"map", tileWidth: 100, tileHeight: 100});
+const map = this.make.tilemap({key:"map", tileWidth: 32, tileHeight: 32});
 const tileset = map.addTilesetImage("wholesheet1", "tiles");
 const layer = map.createLayer("Ground", tileset, 0, 0);
 const layer2 = map.createLayer("Walls", tileset, 0, 0);
+//this.physics.add.collider(this.player, this.wallsLayer);
 }
 update() {
 
