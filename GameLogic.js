1292bc0b801d4ce5c323cf72448dafae7665efe7
phamrina <phamrina@mail.uc.edu>
2022-04-14 14:43:33 -0400

    introduce phaser 3

--- GameLogic.js
+++ GameLogic.js
@@ -4,17 +4,23 @@ let game = new Phaser.Game(800, 800, Phaser.CANVAS,
     'phaser-example', { preload: preload, create: create, update: update });
 
 function preload(){
-    game.load.image(`player`,  `Sprites/player.png`);
+    game.load.image(`player`,  "Sprites/player.png"),
+    {framewidth: 32, frameheight: 32};
+    game.load.image('enemy', "Sprites/enemy.png"),
+    {framewidth: 32, frameheight: 32};
+    game.load.image(`coinobjective`, "Sprites/coinobjective"),
+    {framewidth: 32, frameheight:32};
 }
 
 function create(){
-    game.physics.startSystem(Phaser.Physics.P2JS);
-    
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     player = game.add.sprite(400,400, `player`);
-    
+    enemy = game.add.sprite(250,250, 'enemy');
     cursors = game.input.keyboard.createCursorKeys();
 
 }
+
+//player parameters
 
 
 function update() {
