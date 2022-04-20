a35c8ff918204ced1f9f0d4ef7f77cd4c93fa72c
SamuelUntener <75641765+SamuelUntener@users.noreply.github.com>
2022-04-20 14:57:18 -0400

    it kinda works now

--- Scene1.js
+++ Scene1.js
@@ -5,9 +5,9 @@ class Scene1 extends Phaser.Scene {
 
  preload() {
      this.load.image("tutorial", "Tiles/tutorial.png");
-//     this.load.spritesheet("coinobjective", "src/Sprites/coinobjective");
-//     this.load.spritesheet("enemy", "src/Sprites/enemy.png");
-//     this.load.spritesheet("player", "src/Sprites/player.png");
+     this.load.image("coinobjective", "src/Sprites/coinobjective");
+     this.load.image("enemy", "Sprites/enemy.png");
+     this.load.image("player", "Sprites/player.png");
  }
 create() {
     this.add.text( 20, 20, "Loading game...");
--- Scene2.js
+++ Scene2.js
@@ -9,10 +9,88 @@ create() {
     this.background.setOrigin(0,0);
 
     this.player = this.add.image(config.width - 160, config.height / 2, "player" );
+    this.enemy = this.add.image(config.width - 80, config.height / 2, "enemy" );
+    this.enemy1 = this.add.image(config.width + 80, config.height / 2, "enemy" );
+    this.enemy2 = this.add.image(config.width - 40, config.height / 2, "enemy" );
 
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    //let walls=1;
     //this.add.text(20,20,"Playing Game", {font: "25px Arial", fill: "yellow"});
 }
-update() {
-  
+
+moveEnemy (enemy, speed){
+    enemy.y += speed;
+    if (enemy.y > config.height) {
+        this.resetEnemyposition(enemy);
+    }
 }
+moveEnemy1 (enemy1, speed){
+    enemy1.y += speed;
+    if (enemy1.y > config.height) {
+        this.resetEnemy1position(enemy1);
+    }
+}
+moveEnemy2 (enemy2, speed){
+    enemy2.x += speed;
+    if (enemy2.x > config.height) {
+        this.resetEnemy2position(enemy2);
+    }
+}
+
+resetEnemyposition(enemy){
+    enemy.y = 0;
+    var randomX = Phaser.Math.Between(0, config.width);
+    enemy.x = randomX;
+}
+resetEnemy1position(enemy1){
+    enemy1.y = 0;
+    var randomX = Phaser.Math.Between(0, config.width);
+    enemy1.x = randomX;
+}
+resetEnemy2position(enemy2){
+    enemy2.x = 0;
+    var randomY = Phaser.Math.Between(0, config.height);
+    enemy2.y = randomY;
+}
+
+update() {
+    this.moveEnemy(this.enemy, 5);
+    this.moveEnemy1(this.enemy1, 5);
+    this.moveEnemy2(this.enemy2, 5);
+    this.movePlayerManager();
+ }
+
+ movePlayerManager(){
+     if(this.cursorKeys.left.isDown){
+         this.player.setVelocityX(-gameSettings.playerSpeed);
+     }else if (this.cursorKeys.right.isDown){
+         this.player.setVelocityX(gameSettings.playerSpeed);
+     }
+ }
+
+ 
+    // if (enemy.y >= config.height){
+    //     walls = 1;
+    // }
+    // if (enemy.y <= config.height){
+    //     walls = 2;
+    // }
+    // switch (walls) {
+    //     case 1:
+    //         moveEnemy (enemy, speed){
+    //             enemy.y -= speed;
+    //         }
+    //         break;
+    
+    //     case 2:
+    //         moveEnemy (enemy, speed){
+    //             enemy.y += speed;
+    //         }
+    //         break;
+    //     default:
+    //         moveEnemy (enemy, speed){
+    //             enemy.y -= speed;
+    //         }
+    
+    // }
 }
